1278e4450450c93f15cce60ccea7eb5dc32207c2
Ravindra <ravindra.bankapur@sakhatech.com>
2018-07-18 12:34:16 +0530

    basic components are updated

--- orchid-app-client/src/index.js
+++ orchid-app-client/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Container from './components/container';
+import Header from './components/header';
+import Footer from './components/footer';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Container />, document.getElementById('root'));
 registerServiceWorker();
